3935a36423ee4b3df3832e04cfe627a0a1d327e2
muteebhat <muteebhat7@gmail.com>
2023-10-28 03:07:49 +0530

    -HIt function added -metadata funcation added

--- app.py
+++ app.py
@@ -2,9 +2,8 @@ from flask import Flask, request, jsonify, render_template
 import pyshorteners
 
 app = Flask(__name__)
-url_store = []  # Dictionary to store URL mappings
+urls = [] 
 
-# Create API - Shorten a URL
 @app.route('/shorten', methods=['POST'])
 def shorten_url():
     data = request.get_json()
@@ -13,35 +12,50 @@ def shorten_url():
     
     if not title or not long_url:
         return jsonify({"error": "Both title and long URL are required"}), 400
-    print('URL STORE', url_store)
+    print('URL STORE', urls)
     short_url = shorten(long_url)
-    url_store.append({"title": title, "long_url": long_url, "short_url": short_url})
+    urls.append({"title": title, "long_url": long_url, "short_url": short_url, "hits" : 0})
     return jsonify({"title": title, "short_url": short_url})
 
-# API - Search for URLs
 @app.route('/search', methods=['GET'])
 def search_url():
     data = request.get_json()
     term = data.get('term')
     print('TERM', term)
+    print(urls)
     results = []
-    for item in url_store:
+    for item in urls:
         if term.lower() in item['title'].lower():
             results.append(item)
     
     return jsonify(results)
 
 
-# Web Interface
-@app.route('/')
-def home():
-    return "Welcome to the URL Shortener Service"
 
-# Helper function to shorten a URL using pyshorteners
 def shorten(long_url):
     s = pyshorteners.Shortener()
     return s.tinyurl.short(long_url)
 
+@app.route('/hit', methods=['POST'])
+def increment_hits():
+    short_url = request.get_json().get('short_url')
+
+    for item in urls:
+        if item['short_url'] == short_url:
+            item['hits'] += 1
+            return jsonify({"message": "Hit count incremented for " + short_url})
+
+    return jsonify({"error": "Short URL not found"}), 404
+    
+@app.route('/get-metadata', methods=['GET'])
+def get_metadata():
+    short_url = request.get_json().get('short_url')
+
+    for item in urls:
+        if item['short_url'] == short_url:
+            return jsonify(item)
+
+    return jsonify({"error": "Short URL not found"}), 404
     
 if __name__ == '__main__':
     app.run(debug=True)
